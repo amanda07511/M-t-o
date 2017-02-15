a003631764e6bc170b487671d7bd790bb6af6eda
Amanda Michelle Marroquin Delgado <marroqua@iut1i-605-z09.iut1.local>
2017-02-15 10:24:39 +0100

    Initial Commit

---
+++ MétéoTests/Me_te_oTests.swift
@@ -0,0 +1,36 @@
+//
+//  Me_te_oTests.swift
+//  MétéoTests
+//
+//  Created by Amanda Michelle Marroquin Delgado on 15/02/2017.
+//  Copyright © 2017 Amanda Michelle Marroquin Delgado. All rights reserved.
+//
+
+import XCTest
+@testable import Me_te_o
+
+class Me_te_oTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
